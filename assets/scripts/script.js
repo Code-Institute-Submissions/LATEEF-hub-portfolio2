ae83947ed498ade5dfdd55bd931a082a32abc580
LATEEF-hub <latifkk@krok.edu.ua>
2023-10-01 06:50:13 +0100

    add chart library and select all element

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+//Select Elements
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const expenseTotalEl = document.querySelector(".expense-total");
+const incomeEl = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#expense .list");
+const allList = document.querySelector("#all .list");
+
+//Select BTNS
+
+// SELECT BTNS
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
+
+//INPUT BTNS
+const addExpense = document.querySelector(".add-expense");
+const expenseTitle = document.getElementById("expense-title-input");
+const expenseAmount = document.getElementById("expense-amount-input");
