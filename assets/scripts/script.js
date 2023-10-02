23059bcb7b283b9937e25d041ed253dbe0aad1d4
LATEEF-hub <latifkk@krok.edu.ua>
2023-10-02 07:28:06 +0100

    fixed the bug on function showEntry

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -52,78 +52,113 @@ allBtn.addEventListener("click", function () {
    inactive([incomeBtn, expenseBtn]);
 })
 
-addExpense.addEventListener("click", function(){
+addExpense.addEventListener("click", function () {
    // IF ONE OF THE INPUTS IS EMPTY => EXIT
-   if(!expenseTitle.value || !expenseAmount.value ) return;
+   if (!expenseTitle.value || !expenseAmount.value) return;
 
    // SAVE THE ENTRY TO ENTRY_LIST
    let expense = {
-       type : "expense",
-       title : expenseTitle.value,
-       amount : parseInt(expenseAmount.value)
+      type: "expense",
+      title: expenseTitle.value,
+      amount: parseInt(expenseAmount.value)
    }
    ENTRY_LIST.push(expense);
 
    updateUI();
-   clearInput( [expenseTitle, expenseAmount] )
+   clearInput([expenseTitle, expenseAmount])
 })
 
-addIncome.addEventListener("click", function(){
+addIncome.addEventListener("click", function () {
    // IF ONE OF THE INPUTS IS EMPTY => EXIT
-   if(!incomeTitle.value || !incomeAmount.value ) return;
+   if (!incomeTitle.value || !incomeAmount.value) return;
 
    // SAVE THE ENTRY TO ENTRY_LIST
    let income = {
-       type : "income",
-       title : incomeTitle.value,
-       amount : parseInt(incomeAmount.value)
+      type: "income",
+      title: incomeTitle.value,
+      amount: parseInt(incomeAmount.value)
    }
    ENTRY_LIST.push(income);
 
    updateUI();
-   clearInput( [incomeTitle, incomeAmount] )
+   clearInput([incomeTitle, incomeAmount])
 })
 
-// incomeList.addEventListener("click", deleteOrEdit);
-// expenseList.addEventListener("click", deleteOrEdit);
-// allList.addEventListener("click", deleteOrEdit);
+incomeList.addEventListener("click", deleteOrEdit);
+expenseList.addEventListener("click", deleteOrEdit);
+allList.addEventListener("click", deleteOrEdit);
+
+// HELPERS
+
+function deleteOrEdit(event) {
+   const targetBtn = event.target;
+
+   const entry = targetBtn.parentNode;
+
+   if (targetBtn.id == DELETE) {
+      deleteEntry(entry);
+   } else if (targetBtn.id == EDIT) {
+      editEntry(entry);
+   }
+}
+
+function deleteEntry(entry) {
+   ENTRY_LIST.splice(entry.id, 1);
+
+   updateUI();
+}
+
+function editEntry(entry) {
+   console.log(entry)
+   let ENTRY = ENTRY_LIST[entry.id];
+
+   if (ENTRY.type == "income") {
+      incomeAmount.value = ENTRY.amount;
+      incomeTitle.value = ENTRY.title;
+   } else if (ENTRY.type == "expense") {
+      expenseAmount.value = ENTRY.amount;
+      expenseTitle.value = ENTRY.title;
+   }
+
+   deleteEntry(entry);
+}
 
 
-function updateUI(){
+function updateUI() {
    income = calculateTotal("income", ENTRY_LIST);
-   expense = calculateTotal("expense", ENTRY_LIST);
-   balance = Math.abs(calculateBalance(income, expense));
+   outcome = calculateTotal("expense", ENTRY_LIST);
+   balance = Math.abs(calculateBalance(income, outcome));
 
    // DETERMINE SIGN OF BALANCE
-   let sign = (income >= expense) ? "€" : "-€";
+   let sign = (income >= outcome) ? "€" : "-€";
 
    // UPDATE UI
    balanceEl.innerHTML = `<small>${sign}</small>${balance}`;
-   expenseTotalEl.innerHTML = `<small>$</small>${expense}`;
-   incomeTotalEl.innerHTML = `<small>$</small>${income}`;
+   outcomeTotalEl.innerHTML = `<small>€</small>${outcome}`;
+   incomeTotalEl.innerHTML = `<small>€</small>${income}`;
 
-   clearElement( [expenseList, incomeList, allList] );
+   clearElement([expenseList, incomeList, allList]);
 
-   ENTRY_LIST.forEach( (entry, index) => {
-       if( entry.type == "expense" ){
-           showEntry(expenseList, entry.type, entry.title, entry.amount, index)
-       }else if( entry.type == "income" ){
-           showEntry(incomeList, entry.type, entry.title, entry.amount, index)
-       }
-       showEntry(allList, entry.type, entry.title, entry.amount, index)
+   ENTRY_LIST.forEach((entry, index) => {
+      if (entry.type == "expense") {
+         showEntry(expenseList, entry.type, entry.title, entry.amount, index)
+      } else if (entry.type == "income") {
+         showEntry(incomeList, entry.type, entry.title, entry.amount, index)
+      }
+      showEntry(allList, entry.type, entry.title, entry.amount, index)
    });
 
-   updateChart(income, expense);
+   updateChart(income, outcome);
 
    localStorage.setItem("entry_list", JSON.stringify(ENTRY_LIST));
 }
 
 
 //Function to show Entry
-function showEntry(list, type, title, amount, id){
+function showEntry(list, type, title, amount, id) {
 
    const entry = ` <li id = "${id}" class="${type}">
-                       <div class="entry">${title}: $${amount}</div>
+                       <div class="entry">${title}: € ${amount}</div>
                        <div id="edit"></div>
                        <div id="delete"></div>
                    </li>`;
@@ -133,33 +168,33 @@ function showEntry(list, type, title, amount, id){
    list.insertAdjacentHTML(position, entry);
 }
 
-function clearElement(elements){
-   elements.forEach( element => {
-       element.innerHTML = "";
+function clearElement(elements) {
+   elements.forEach(element => {
+      element.innerHTML = "";
    })
 }
 
-// function calculateTotal(type, list){
-//    let sum = 0;
+function calculateTotal(type, list) {
+   let sum = 0;
 
-//    list.forEach( entry => {
-//        if( entry.type == type ){
-//            sum += entry.amount;
-//        }
-//    })
+   list.forEach(entry => {
+      if (entry.type == type) {
+         sum += entry.amount;
+      }
+   })
 
-//    return sum;
-// }
+   return sum;
+}
 
-// function calculateBalance(income, expense){
-//    return income - expense;
-// }
+function calculateBalance(income, expense) {
+   return income - outcome;
+}
 
-// function clearInput(inputs){
-//    inputs.forEach( input => {
-//        input.value = "";
-//    })
-// }
+function clearInput(inputs) {
+   inputs.forEach(input => {
+      input.value = "";
+   })
+}
 
 
 function show(element) {
