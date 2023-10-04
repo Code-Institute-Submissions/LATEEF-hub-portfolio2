c395e9ebda9b0c93b811c16b5242b77d96761d53
LATEEF-hub <latifkk@krok.edu.ua>
2023-10-04 03:35:52 +0100

    made final changes to code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,4 @@
-// Hello.
-//
-// This is JSHint, a tool that helps to detect errors and potential
-// problems in your JavaScript code.
-//
-// To start, simply enter some JavaScript anywhere on this page. Your
-// report will appear on the right side.
-//
-// Additionally, you can toggle specific options in the Configure
-// menu.
 
-//Select Elements
 const balanceEl = document.querySelector(".balance .value");
 const incomeTotalEl = document.querySelector(".income-total");
 const outcomeTotalEl = document.querySelector(".outcome-total");
